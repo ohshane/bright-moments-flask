9cfcec514e17f9f0da501f6d029e3a677f32346f
Shane Oh <ohshane71@gmail.com>
2021-07-18 19:49:52 +0900

    added requirements.txt

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import cv2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
+camera = cv2.VideoCapture(0)  # use 0 for web camera
+# for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
 def gen_frames():  # generate frame by frame from camera
