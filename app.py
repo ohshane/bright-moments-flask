c97889e6cdfe74f037f7b7ee3ea8834f5e6f0b92
Shane Oh <ohshane71@gmail.com>
2021-07-27 13:15:11 +0900

    working flask cv2 modified:   app.py

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 from flask import Flask, render_template, Response
 import cv2
-from cascade import detectAndDisplay
+from cascade import *
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)  # use 0 for web camera
+# camera = cv2.VideoCapture(0)  # use 0 for web camera
 # for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
 def gen_frames():  # generate frame by frame from camera
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            frame = detectAndDisplay(frame)
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    # while True:
+    #     # Capture frame-by-frame
+    #     success, frame = camera.read()  # read the camera frame
+    #     if not success:
+    #         break
+    #     else:
+    #         frame = detectAndDisplay(frame)
+    #         ret, buffer = cv2.imencode('.jpg', frame)
+    #         frame = buffer.tobytes()
+    #         yield (b'--frame\r\n'
+    #                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened:
+        print('--(!)Error opening video capture')
+        exit(0)
+    while True:
+        ret, frame = cap.read()
+        if frame is None:
+            print('--(!) No captured frame -- Break!')
+            break
+        detectAndDisplay(frame)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+        # if cv.waitKey(10) == 27:
+        #     break
 
 @app.route('/video_feed')
 def video_feed():
